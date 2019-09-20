7f2eb0d4f70bad8b82d00b2f67bd835d31829c0a
Hardy Griesbauer <hardy.griesbauer@gov.bc.ca>
2019-09-20 16:57:42 -0700

    asd

---
+++ R/pliDat.R
@@ -0,0 +1,20 @@
+#' Lodgepole pine mortality data
+#'
+#' @docType data
+#'
+#' @usage data(pliDat)
+#'
+#'@format A data frame with 92 rows and 15 variables:
+#' \describe{
+#'   \item{Plot}{Plot # within opening, always two trees per plot}
+#'   \item{Tree #}{Tree #1 is always the dead tree, Tree #2 is live}
+#'   \item{Distance to live partner}{Distance between trees in plot, m}
+#'   \item{Crown class}{Dominant or codominant trees were sampled}
+#'   \item{Crown volume}{This compares volume between live and dead trees within a plot.}
+#'   THe live tree always has a Crown volume of 100%, and the dead tree is a percentage of that.}
+#'   \item{Longest branch}{Ocular estimate of longest branch within crown, cm}
+#'   \item{B}{Height to bottom of crown (in case of dead trees, where live crown was),m}
+#'   \item{Percentage of live needles}{Percentage of live needles to dead needles in crown}
+#'   }
+#' @keywords datasets
+"pliDat"
