cef039cbf6d4516c38482ae49fc4c0f135faa1e8
Hardy Griesbauer <hardy.griesbauer@gov.bc.ca>
2019-09-20 17:08:02 -0700

    sdf

--- R/pliDat.R
+++ R/pliDat.R
@@ -11,7 +11,6 @@
 #'   \item{Distance to live partner}{Distance between trees in plot, m}
 #'   \item{Crown class}{Dominant or codominant trees were sampled}
 #'   \item{Crown volume}{This compares volume between live and dead trees within a plot.}
-#'   THe live tree always has a Crown volume of 100%, and the dead tree is a percentage of that.}
 #'   \item{Longest branch}{Ocular estimate of longest branch within crown, cm}
 #'   \item{B}{Height to bottom of crown (in case of dead trees, where live crown was),m}
 #'   \item{Percentage of live needles}{Percentage of live needles to dead needles in crown}
